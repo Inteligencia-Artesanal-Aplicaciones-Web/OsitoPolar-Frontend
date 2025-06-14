a5b134f0e36770c6efd7d3b8f977c4263f409b7a
eJos-R <joseph.rodriguez.parco@outlook.com>
2025-06-13 21:22:10 -0500

    feat(rental): add entity classes for rental contracts, equipment, payments, prices, and requests

---
+++ src/rental/models/rental-contract.entity.js
@@ -0,0 +1,39 @@
+﻿/**
+ * @class RentalContract
+ * @description Active rental contract after approval
+ */
+export class RentalContract {
+    constructor({
+                    id = '',
+                    userId = '',
+                    rentalRequestId = '',
+                    contractNumber = '',
+                    startDate = '',
+                    endDate = '',
+                    monthlyPrice = 0,
+                    status = 'active', // active, paused, terminated, completed
+                    paymentMethod = '', // stripe_card, bank_transfer
+                    stripeCustomerId = '',
+                    stripeSubscriptionId = '',
+                    equipmentDelivered = false,
+                    deliveryDate = '',
+                    createdAt = '',
+                    updatedAt = ''
+                }) {
+        this.id = id;
+        this.userId = userId;
+        this.rentalRequestId = rentalRequestId;
+        this.contractNumber = contractNumber;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.monthlyPrice = monthlyPrice;
+        this.status = status;
+        this.paymentMethod = paymentMethod;
+        this.stripeCustomerId = stripeCustomerId;
+        this.stripeSubscriptionId = stripeSubscriptionId;
+        this.equipmentDelivered = equipmentDelivered;
+        this.deliveryDate = deliveryDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
