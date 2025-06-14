6822fc11286854b49d3035c52fb12169af5a5be5
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-14 05:14:06 -0500

    fix(index): refactor routes array.

--- src/router/index.js
+++ src/router/index.js
@@ -32,25 +32,45 @@ const PageNotFoundComponent    = () => import('../public/pages/page-not-found.co
  */
 const routes = [
     // Public module
-    { path: '/home', name: 'home',            component: HomeComponent,            meta: { title: 'Home' } },
+    {path: '/home', name: 'home', component: HomeComponent, meta: {title: 'Home'}},
     // About
-    { path: '/about',           name: 'about',           component: AboutComponent,           meta: { title: 'About us' } },
+    {path: '/about', name: 'about', component: AboutComponent, meta: {title: 'About us'}},
     // Notifications
-    {path: '/notifications', name: 'notifications',   component: NotificationsComponent,   meta: { title: 'Notifications' } },
+    {path: '/notifications', name: 'notifications', component: NotificationsComponent, meta: {title: 'Notifications'}},
     // Dashboard
-    { path: '/dashboard',       name: 'dashboard',       component: DashboardComponent,       meta: { title: 'Dashboard' } },
+    {path: '/dashboard', name: 'dashboard', component: DashboardComponent, meta: {title: 'Dashboard'}},
     //Equipment analytics
-    {path: '/equipment/:id/analytics', name: 'equipment-analytics', component: EquipmentAnalyticsComponent, meta: { title: 'Equipment Analytics' } },
+    {
+        path: '/equipment/:id/analytics',
+        name: 'equipment-analytics',
+        component: EquipmentAnalyticsComponent,
+        meta: {title: 'Equipment Analytics'}
+    },
     // Equipment module
-    { path: '/equipment',              name: 'equipment-list',       component: EquipmentListComponent,     meta: { title: 'My Equipment' } },
-    { path: '/equipment/:id',          name: 'equipment-detail',     component: EquipmentDetailComponent,   meta: { title: 'Equipment Control' } },
+    {path: '/equipment', name: 'equipment-list', component: EquipmentListComponent, meta: {title: 'My Equipment'}},
+    {
+        path: '/equipment/:id',
+        name: 'equipment-detail',
+        component: EquipmentDetailComponent,
+        meta: {title: 'Equipment Control'}
+    },
     // Service Requests
-    { path: '/service-requests',       name: 'service-requests',     component: ServiceRequestListComponent, meta: { title: 'Service Requests' } },
-    { path: '/service-request/new',    name: 'new-service-request',  component: NewServiceRequestComponent, meta: { title: 'New Service Requests' } },
+    {
+        path: '/service-requests',
+        name: 'service-requests',
+        component: ServiceRequestListComponent,
+        meta: {title: 'Service Requests'}
+    },
+    {
+        path: '/service-request/new',
+        name: 'new-service-request',
+        component: NewServiceRequestComponent,
+        meta: {title: 'New Service Requests'}
+    },
     // Default route
-    { path: '/',                       name: 'default',              redirect: { name: 'home' } },
+    {path: '/', name: 'default', redirect: {name: 'home'}},
     // Not found route
-    { path: '/:pathMatch(.*)*',        name: 'not-found',            component: PageNotFoundComponent,      meta: { title: 'Page not found' } },
+    {path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFoundComponent, meta: {title: 'Page not found'}},
 ];
 
 /**
