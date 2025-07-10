2d8deb2653a56dfe4654af0ec228059e96d0956e
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-07-09 22:36:36 -0500

    feat(user): create User entity class to represent user data model

---
+++ src/iam/models/user.entity.js
@@ -0,0 +1,24 @@
+﻿/**
+ * @class User
+ * @description Entity class representing a user, aligned with the backend model.
+ */
+export class User {
+    /**
+     * @param {Object} [data={}] - Raw data object from the API response
+     * @param {number} [data.id=0] - Unique identifier
+     * @param {string} [data.username=''] - Username
+     * @param {string} [data.createdAt=null] - Creation timestamp
+     * @param {string} [data.updatedAt=null] - Last update timestamp
+     */
+    constructor({
+                    id = 0,
+                    username = '',
+                    createdAt = null,
+                    updatedAt = null
+                } = {}) {
+        this.id = id;
+        this.username = username;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
