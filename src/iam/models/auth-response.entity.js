e898f5f175223aa3892d0f74ad2729aeeb7e98f9
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-07-09 22:35:14 -0500

    feat(auth-response): add AuthResponse entity class to represent authentication response

---
+++ src/iam/models/auth-response.entity.js
@@ -0,0 +1,21 @@
+﻿/**
+ * @class AuthResponse
+ * @description Entity class representing authentication response from backend
+ */
+export class AuthResponse {
+    /**
+     * @param {Object} [data={}] - Raw data object from the API response
+     * @param {number} [data.id=0] - User ID
+     * @param {string} [data.username=''] - Username
+     * @param {string} [data.token=''] - JWT token
+     */
+    constructor({
+                    id = 0,
+                    username = '',
+                    token = ''
+                } = {}) {
+        this.id = id;
+        this.username = username;
+        this.token = token;
+    }
+}
